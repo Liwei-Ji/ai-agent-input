0331e096c0dd0e93ba92275efc5f70f5b803444d
Liwei Ji <jpw0616@gmail.com>
2026-01-29 19:07:04 +0800

    Add description for cards

--- src/cards.ts
+++ src/cards.ts
@@ -127,27 +127,47 @@ export const data: CardGroup[] = [
       {
         title: "Restyle",
         subtitle: "Transfer styles without changing the underlying structure of a generation",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b35f00d5707febd832b3ca_restyle_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b35f00d5707febd832b3ca_restyle_card.svg",
+        description: [
+      "restyle_cta.desc_1",
+      "restyle_cta.desc_2"
+    ]
       },
       {
         title: "Summary",
         subtitle: "Have AI distill a topic or resource down to its essence",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68acf919f39d0154b592c46e_summary_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68acf919f39d0154b592c46e_summary_card.svg",
+        description: [
+      "summary_cta.desc_1",
+      "summary_cta.desc_2"
+    ]
       },
       {
         title: "Synthesis",
         subtitle: "Distill or reorganize complicated information into simple structure",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68acf9d7f5349d41aa54b92b_synthesize_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68acf9d7f5349d41aa54b92b_synthesize_card.svg",
+        description: [
+      "synthesis_cta.desc_1",
+      "synthesis_cta.desc_2"
+    ]
       },
       {
         title: "Token layering",
         subtitle: "Construct a prompt with raw tokens, just like building with legos",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b371846133444d3059a978_tokenlayering_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b371846133444d3059a978_tokenlayering_card.svg",
+        description: [
+      "token_layering_cta.desc_1",
+      "token_layering_cta.desc_2"
+    ]
       },
       {
         title: "Transform",
         subtitle: "Use AI to change the modality of content",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b35ec8b17834050ebc98e5_transform_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b35ec8b17834050ebc98e5_transform_card.svg",
+        description: [
+      "transform_cta.desc_1",
+      "transform_cta.desc_2"
+    ]
       }
     ]
   },
@@ -159,47 +179,59 @@ export const data: CardGroup[] = [
       {
         title: "Collapse Input",
         subtitle: "Improve focus and layout efficiency by collapsing the input area while preserving all input content.",
-        img: ""
+        img: "",
+        description: [
+      "collapse_input_cta.desc_1",
+      "collapse_input_cta.desc_2"
+    ]
       },
       {
         title: "Attachments",
         subtitle: "Give the AI a specific reference to anchor its response",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1b90f5349d41aa5ae38a_attachments_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1b90f5349d41aa5ae38a_attachments_card.svg",
+        description: [
+      "attachments_cta.desc_1",
+      "attachments_cta.desc_2"
+    ]
       },
       {
         title: "Filters",
         subtitle: "Constrain the inputs or the outputs of the AI by source, type, modality, etc",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad07d23eec6f119675d709_filters_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad07d23eec6f119675d709_filters_card.svg",
+              description: [
+      "attachments_cta.desc_1",
+      "attachments_cta.desc_2"
+    ]
       },
       {
         title: "Inpainting",
         subtitle: "Target specific areas of the AI's result to regenerate or remix",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad10e7000b95f2b577f9fb_inpainting_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad10e7000b95f2b577f9fb_inpainting_card.svg",
       },
       {
         title: "Model management",
         subtitle: "Let users specify what model to use for their prompts",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1231271efefc27673d31_modelmanagement_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1231271efefc27673d31_modelmanagement_card.svg",
       },
       {
         title: "Parameters",
         subtitle: "Include constraints with your prompt for the AI to reference when generating its result",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad155b9b8d80f64aebee1e_parameters_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad155b9b8d80f64aebee1e_parameters_card.svg",
       },
       {
         title: "Personal voice",
         subtitle: "Ensure outputs match your voice, tone, and preferences in a consistent way",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad15f5719956709202c6e2_personalovoice_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad15f5719956709202c6e2_personalovoice_card.svg",
       },
       {
         title: "References",
         subtitle: "See and manage what additional sources the AI references to generate its response",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1cf4e65278d06e72d111_references_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1cf4e65278d06e72d111_references_card.svg",
       },
       {
         title: "Workflows",
         subtitle: "String generative steps together to synthesize, create, or send content on auto-pilot",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1fe8e715687380252c4e_workflows_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad1fe8e715687380252c4e_workflows_card.svg",
       }
     ]
   },
@@ -211,22 +243,22 @@ export const data: CardGroup[] = [
       {
         title: "Citations",
         subtitle: "Give the AI a specific reference to anchor its response",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac7be5d23c1be315c14fc4_citation_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac7be5d23c1be315c14fc4_citation_card.svg",
       },
       {
         title: "Controls",
         subtitle: "Manage the flow of information or pause a request mid-stream to adjust the prompt",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac7ee1bea66470b3c8339e_controls_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac7ee1bea66470b3c8339e_controls_card.svg",
       },
       {
         title: "Footprints",
         subtitle: "Let users trace the AI's steps from prompt to result",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac832f83a5ddd9aab906d3_footprints_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac832f83a5ddd9aab906d3_footprints_card.svg",
       },
       {
         title: "Plan of Action",
         subtitle: "Have the AI show the steps it will take to respond to the user's prompt before it executes its response",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac95a1b099d421f1be4c78_planofaction_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac95a1b099d421f1be4c78_planofaction_card.svg",
       },
       {
         title: "Stream Of Thought",
@@ -236,32 +268,32 @@ export const data: CardGroup[] = [
       {
         title: "Prompt transparency",
         subtitle: "Show users what is actually happening behind the scenes",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac6c5a16f4eb679b0dbc5f_prompttransparency_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac6c5a16f4eb679b0dbc5f_prompttransparency_card.svg",
       },
       {
         title: "Regenerate",
         subtitle: "Have the AI reproduce its response to the prompt without additional input",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac864d563bf2b258221d00_regenerate_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac864d563bf2b258221d00_regenerate_card.svg",
       },
       {
         title: "Copy",
         subtitle: "Instantly copy the response to your clipboard.",
-        img: ""
+        img: "",
       },
       {
         title: "Sample response",
         subtitle: "Confirm the user's intent for complicated prompts",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac90cc404c75f50377f40f_sampleresponse_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac90cc404c75f50377f40f_sampleresponse_card.svg",
       },
       {
         title: "Token Transparency",
         subtitle: "Reveal the tokens the AI used to craft its response",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac5fc7c03253880a417461_tokentransparency_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac5fc7c03253880a417461_tokentransparency_card.svg",
       },
       {
         title: "Variations",
         subtitle: "Trace through multiple variations of a result to choose the one that works best for them",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac9935f14f97dac35b004c_variations_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ac9935f14f97dac35b004c_variations_card.svg",
       }
     ]
   },
@@ -273,22 +305,22 @@ export const data: CardGroup[] = [
       {
         title: "Consent",
         subtitle: "Only capture data from others with their knowledge and permission",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b617c289680bc8e194b949_consent_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b617c289680bc8e194b949_consent_card.svg",
       },
       {
         title: "Watermark",
         subtitle: "Identifiers on AI Generative content that humans, software, or programs can read",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad3102cabb29e3abc06f3d_watermarks_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad3102cabb29e3abc06f3d_watermarks_card.svg",
       },
       {
         title: "Memory",
         subtitle: "Control what details the AI knows about you",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad2e956ec69fa4ae8385c2_memory_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad2e956ec69fa4ae8385c2_memory_card.svg",
       },
       {
         title: "Interact with the AI without leaving any traces",
         subtitle: "Large, open-ended input inviting the user to start their first interaction with the AI",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad2dc30a9b0bee5768c764_incognito_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad2dc30a9b0bee5768c764_incognito_card.svg",
       }
     ]
   },
@@ -300,17 +332,17 @@ export const data: CardGroup[] = [
       {
         title: "Caveat",
         subtitle: "Inform users about shortcomings or risks in the model or the technology overall",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad33551d4f4f54c24f6054_caveat_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad33551d4f4f54c24f6054_caveat_card.svg",
       },
       {
         title: "Rating",
         subtitle: "Signal expectation gaps or errors in the model – but is that clear to the user?",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/66550b496ae3ac9e19b0d9a2_p_rating.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/66550b496ae3ac9e19b0d9a2_p_rating.svg",
       },
       {
         title: "Data ownership",
         subtitle: "Control how the model remembers and uses your data",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad348d3ddb47cd6b8c56bd_dataownership_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68ad348d3ddb47cd6b8c56bd_dataownership_card.svg",
       }
     ]
   },
@@ -322,26 +354,34 @@ export const data: CardGroup[] = [
       {
         title: "Color",
         subtitle: "Visual cues to help users identify AI features or content",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b331d407f299d4789625e6_color_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b331d407f299d4789625e6_color_card.svg",
       },
       {
         title: "Disclosure",
         subtitle: "Clearly mark content and interactions guided or delivered by AI",
         img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b3319ed0bdb1335b8f7c20_disclosure_card.svg",
         description: [
-      "disclosure.desc_1",
-      "disclosure.desc_2"
+      "disclosure_desc_1",
+      "disclosure_desc_2"
      ]
       },
       {
         title: "Name",
         subtitle: "How do we refer to the AI?",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b331d407f299d4789625df_name_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b331d407f299d4789625df_name_card.svg",
+        description: [
+      "name_desc_1",
+      "name_desc_2"
+     ]
       },
       {
         title: "Personality",
         subtitle: "Characteristics that distinguish the AI's personality and vibe",
-        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b3319fd0bdb1335b8f7c35_personality_card.svg"
+        img: "https://cdn.prod.website-files.com/65db6dd21591364dfcb8ae36/68b3319fd0bdb1335b8f7c35_personality_card.svg",
+        description: [
+      "personality_desc_1",
+      "personality_desc_2"
+     ]
       }
     ]
   }
